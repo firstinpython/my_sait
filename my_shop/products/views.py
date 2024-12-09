b0bd6693d77cac8ef97affa95d292639ebff47fd
firstinpython <ntr.0707@mail.ru>
2024-12-09 10:40:59 +0300

    view product new

--- my_shop/products/views.py
+++ my_shop/products/views.py
@@ -4,31 +4,32 @@ from .models import Product
 
 
 # Create your views here.
-def get_params(params,products):
+def get_params(params, products):
     fbrand = {}
     for product in products:
         fbrand[product.brand.name] = product.brand.name
     dict_params = {
-        "sort":{
-            "priceup":"-price",
-            "pricedown":"price"
+        "sort": {
+            "priceup": "-price",
+            "pricedown": "price"
         },
-        "fdlvt":{
-            "24":1,
-            "48":2,
-            "72":3
+        "fdlvt": {
+            "24": 1,
+            "48": 2,
+            "72": 3
         },
-        "fbrand":fbrand
+        "fbrand": fbrand
     }
     for key, value in params.items():
+        print(key)
         if key == "sort":
-            if value in [v for v in dict_params[key].values]:
+            if value in [v for v in dict_params[key]]:
                 products = products.order_by(dict_params[key][value])
-        if key == "dlvt":
-            if value in [v for v in dict_params[key].values]:
+        if key == "fdlvt":
+            if value in [v for v in dict_params[key]]:
                 products = products.filter(delivery_time__lte=dict_params[key][value])
         if key == "fbrand":
-            if value in [v for v in dict_params[key].values]:
+            if value in [v for v in dict_params[key]]:
                 products = products.filter(brand__name=dict_params[key][value])
 
     return products
@@ -37,6 +38,6 @@ def get_params(params,products):
 def products_view(requests):
     products = Product.objects.all()
     params: dict = requests.GET
-    products_f = get_params(params,products)
+    products_f = get_params(params, products)
     print(products_f)
     return HttpResponse("ok")
